7cc9c9e2a64dc2635595af9f6a99313f5b43d7fe
jihun1233 <leejihun1233@gmail.com>
2022-05-11 16:20:57 +0900

    ✨ 레이아웃, 검색기능 추가

--- src/routes/Search/index.tsx
+++ src/routes/Search/index.tsx
@@ -1,13 +1,34 @@
-import React from 'react'
+import React, { FormEvent, useState } from 'react'
 import styles from './Search.module.scss'
-import {BsSearch} from 'react-icons/bs'
+import { BsSearch } from 'react-icons/bs'
+import { getMovieApi } from 'services/movie'
+import MovieList from 'routes/MovieList'
 
 const Search = (): JSX.Element => {
+  const [searchKeyword, setSearchKeyword] = useState('apple')
+  // 없으면 검색 x, 너무많은 결과 에러에 대응
+  const [page, setPage] = useState(1)
+
+  const getMovieData = () => {
+    getMovieApi({ s: searchKeyword, page }).then(console.log)
+  }
+
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault()
+    getMovieData()
+  }
+
   return (
-    <div>
-      <div className={styles.searchBox}>
-        <BsSearch />
-      </div>
+    <div className={styles.searchPageBox}>
+      <form onSubmit={handleSubmit}>
+        <input type='text' />
+        <button type='submit'>
+          <BsSearch />
+        </button>
+      </form>
+      <section className={styles.movieListBox}>
+        <MovieList />
+      </section>
     </div>
   )
 }
