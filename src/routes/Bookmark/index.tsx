6a9c14791d205aa57328baa71a8ddbb1843a0651
jihun1233 <leejihun1233@gmail.com>
2022-05-15 05:36:33 +0900

    ✨ Bookmark 페이지 컴포넌트 추가
    - 즐겨찾기한 내용을 볼 수 있음
    
    - 별도의 페이징은 없음

--- src/routes/Bookmark/index.tsx
+++ src/routes/Bookmark/index.tsx
@@ -1,7 +1,23 @@
+import MovieItem from 'components/MovieItem'
+import { useRecoil } from 'hooks/state'
+import { bookmarkListState } from 'states/movie'
 import styles from './Bookmark.module.scss'
 
 const Bookmark = (): JSX.Element => {
-  return <div>bookmark</div>
+  const [bookmarks] = useRecoil(bookmarkListState)
+
+  return (
+    <div className={styles.bookmark}>
+      <h1>즐겨찾기</h1>
+      <section className={styles.bookmarkListBox}>
+        <ul>
+          {bookmarks.map((bookmark) => (
+            <MovieItem key={`bookmark ${bookmark.imdbID}`} movie={bookmark} isBookmarked />
+          ))}
+        </ul>
+      </section>
+    </div>
+  )
 }
 
 export default Bookmark
