ec5b960af2de12b32f38782e243aba0184cc5d07
jihun1233 <leejihun1233@gmail.com>
2022-05-14 17:54:28 +0900

    ✨ bookmarkModalState 추가
    - 북마크 모달을 위한 state 추가, bookmark type 변경
    
    - 모달을 열고 내용을 표시하기 위해 추가
    - bookmark에는 order가 필요해서 IMovie에 해당부분 추가하여 새로운 type 생성

--- src/states/movie.ts
+++ src/states/movie.ts
@@ -7,12 +7,29 @@ interface IMovieState extends IMovieResponse {
   keyword: string
 }
 
+interface IBookmark extends IMovie {
+  order: number
+}
+
+interface IBookmarkModalState {
+  isOpen: boolean
+  movie: IMovie
+}
+
 export const movieListState = atom<IMovieState>({
   key: '#movieListState',
   default: { Response: 'False', Search: [], totalResults: 0, Error: '', page: 1, currentMovieList: [], keyword: '' },
 })
 
-export const bookmarkListState = atom<IMovie[]>({
+export const bookmarkListState = atom<IBookmark[]>({
   key: '#bookmarkListState',
   default: [],
+})
+
+export const bookmarkModalState = atom<IBookmarkModalState>({
+  key: '#bookmarkModalState',
+  default: {
+    isOpen: false,
+    movie: { Poster: '', Title: '', Type: '', Year: '', imdbID: '' },
+  },
 })
